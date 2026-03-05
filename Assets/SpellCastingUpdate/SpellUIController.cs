f13c83e60bf327efdce5ec5b4670cf7ccd44cf90
Pangyunfan233 <shuaiyan.chen@ontariotechu.net>
2026-03-05 02:46:19 -0500

    Update SpellUIController.cs
    This is the correct (new spelling)

--- Assets/SpellCastingUpdate/SpellUIController.cs
+++ Assets/SpellCastingUpdate/SpellUIController.cs
@@ -1,56 +1,190 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SimpleSpellUI : MonoBehaviour
+public class SpellUIController : MonoBehaviour
 {
-    public Image iceFill;
-    public Image fireFill;
-    public Image poisonFill;
+    public Image spellIcon;
+    public Image iceCooldownFill;
+    public Image fireCooldownFill;
+    public Image poisonCooldownFill;
 
-    float timer = 0f;
-    float cooldown = 3f;
+    public Sprite iceSprite;
+    public Sprite fireSprite;
+    public Sprite poisonSprite;
 
-    Image currentFill;
+    private float iceSpellCooldown;
+    private float iceSpellTimer;
+    private float fireSpellCooldown;
+    private float fireSpellTimer;
+    private float poisonSpellCooldown;
+    private float poisonSpellTimer;
+
+    private float currentSwapCooldown;
+    private float swapCooldownTimer;
+    private Image activeCooldownFill;
+    private bool isInSwapCooldown = false;
 
     void Start()
     {
-        currentFill = iceFill;
+        iceCooldownFill.fillAmount = 1f;
+        fireCooldownFill.fillAmount = 1f;
+        poisonCooldownFill.fillAmount = 1f;
 
-        iceFill.fillAmount = 1;
-        fireFill.fillAmount = 0;
-        poisonFill.fillAmount = 0;
+        iceCooldownFill.gameObject.SetActive(false);
+        fireCooldownFill.gameObject.SetActive(false);
+        poisonCooldownFill.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        // 切换技能
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchSpell(iceFill);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchSpell(fireFill);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchSpell(poisonFill);
-
-        // 右键使用
-        if (Input.GetMouseButtonDown(1))
+        if (isInSwapCooldown && activeCooldownFill != null)
         {
-            timer = cooldown;
+            swapCooldownTimer -= Time.deltaTime;
+            activeCooldownFill.fillAmount = 1f - (swapCooldownTimer / currentSwapCooldown);
+
+            if (swapCooldownTimer <= 0f)
+            {
+                isInSwapCooldown = false;
+                activeCooldownFill.fillAmount = 1f;
+            }
         }
-
-        // 倒数
-        if (timer > 0)
+        else
         {
-            timer -= Time.deltaTime;
-            currentFill.fillAmount = timer / cooldown;
+            if (iceSpellTimer > 0)
+            {
+                iceSpellTimer -= Time.deltaTime;
+                if (activeCooldownFill == iceCooldownFill)
+                {
+                    iceCooldownFill.fillAmount = 1f - (iceSpellTimer / iceSpellCooldown);
+                }
+            }
+
+            if (fireSpellTimer > 0)
+            {
+                fireSpellTimer -= Time.deltaTime;
+                if (activeCooldownFill == fireCooldownFill)
+                {
+                    fireCooldownFill.fillAmount = 1f - (fireSpellTimer / fireSpellCooldown);
+                }
+            }
+
+            if (poisonSpellTimer > 0)
+            {
+                poisonSpellTimer -= Time.deltaTime;
+                if (activeCooldownFill == poisonCooldownFill)
+                {
+                    poisonCooldownFill.fillAmount = 1f - (poisonSpellTimer / poisonSpellCooldown);
+                }
+            }
         }
     }
 
-    void SwitchSpell(Image newFill)
+    void UpdateSpellCooldownDisplay()
     {
-        if (timer > 0) return; // CD期间不能切换
+        if (activeCooldownFill == iceCooldownFill)
+        {
+            if (iceSpellTimer > 0)
+            {
+                iceCooldownFill.fillAmount = 1f - (iceSpellTimer / iceSpellCooldown);
+            }
+            else
+            {
+                iceCooldownFill.fillAmount = 1f;
+            }
+        }
+        else if (activeCooldownFill == fireCooldownFill)
+        {
+            if (fireSpellTimer > 0)
+            {
+                fireCooldownFill.fillAmount = 1f - (fireSpellTimer / fireSpellCooldown);
+            }
+            else
+            {
+                fireCooldownFill.fillAmount = 1f;
+            }
+        }
+        else if (activeCooldownFill == poisonCooldownFill)
+        {
+            if (poisonSpellTimer > 0)
+            {
+                poisonCooldownFill.fillAmount = 1f - (poisonSpellTimer / poisonSpellCooldown);
+            }
+            else
+            {
+                poisonCooldownFill.fillAmount = 1f;
+            }
+        }
+    }
 
-        iceFill.fillAmount = 0;
-        fireFill.fillAmount = 0;
-        poisonFill.fillAmount = 0;
+    public void ShowSpell(string spellName)
+    {
+        iceCooldownFill.gameObject.SetActive(false);
+        fireCooldownFill.gameObject.SetActive(false);
+        poisonCooldownFill.gameObject.SetActive(false);
 
-        currentFill = newFill;
-        currentFill.fillAmount = 1;
+        switch (spellName)
+        {
+            case "Ice":
+                spellIcon.sprite = iceSprite;
+                activeCooldownFill = iceCooldownFill;
+                iceCooldownFill.gameObject.SetActive(true);
+                break;
+            case "Fire":
+                spellIcon.sprite = fireSprite;
+                activeCooldownFill = fireCooldownFill;
+                fireCooldownFill.gameObject.SetActive(true);
+                break;
+            case "Poison":
+                spellIcon.sprite = poisonSprite;
+                activeCooldownFill = poisonCooldownFill;
+                poisonCooldownFill.gameObject.SetActive(true);
+                break;
+            case "None":
+                spellIcon.sprite = null;
+                activeCooldownFill = null;
+                break;
+        }
+
+        UpdateSpellCooldownDisplay();
+    }
+
+    public void StartSwapCooldown(float cooldown)
+    {
+        if (activeCooldownFill == null)
+        {
+            return;
+        }
+
+        currentSwapCooldown = cooldown;
+        swapCooldownTimer = cooldown;
+        activeCooldownFill.fillAmount = 0f;
+        isInSwapCooldown = true;
+    }
+
+    public void StartSpellCooldown(float cooldown)
+    {
+        if (activeCooldownFill == null)
+        {
+            return;
+        }
+
+        if (activeCooldownFill == iceCooldownFill)
+        {
+            iceSpellCooldown = cooldown;
+            iceSpellTimer = cooldown;
+        }
+        else if (activeCooldownFill == fireCooldownFill)
+        {
+            fireSpellCooldown = cooldown;
+            fireSpellTimer = cooldown;
+        }
+        else if (activeCooldownFill == poisonCooldownFill)
+        {
+            poisonSpellCooldown = cooldown;
+            poisonSpellTimer = cooldown;
+        }
+
+        isInSwapCooldown = false;
+        UpdateSpellCooldownDisplay();
     }
 }
